859d82e4e3579a4ed2d7ffd2f05cc5eac224efd7
Kevin Levron <kevin.levron@gmail.com>
2020-09-16 21:11:23 +0200

    update materials

--- src/materials/Material.js
+++ src/materials/Material.js
@@ -1,10 +1,40 @@
+import { FrontSide } from 'three';
+
 export default {
   inject: ['three'],
   props: {
     id: String,
     color: {
-      type: String,
+      type: [String, Number],
       default: '#ffffff',
+    },
+    depthTest: {
+      type: Boolean,
+      default: true,
+    },
+    depthWrite: {
+      type: Boolean,
+      default: true,
+    },
+    fog: {
+      type: Boolean,
+      default: false,
+    },
+    opacity: {
+      type: Number,
+      default: 1,
+    },
+    side: {
+      type: Number,
+      default: FrontSide,
+    },
+    transparent: {
+      type: Boolean,
+      default: false,
+    },
+    vertexColors: {
+      type: Boolean,
+      default: false,
     },
   },
   mounted() {
@@ -12,6 +42,15 @@ export default {
   },
   unmounted() {
     this.material.dispose();
+  },
+  methods: {
+    propsValues() {
+      const props = {};
+      for (const [key, value] of Object.entries(this.$props)) {
+        if (key !== 'id') props[key] = value;
+      }
+      return props;
+    },
   },
   render() {
     return [];
--- src/materials/StandardMaterial.js
+++ src/materials/StandardMaterial.js
@@ -3,9 +3,25 @@ import Material from './Material';
 
 export default {
   extends: Material,
+  props: {
+    emissive: {
+      type: [Number, String],
+      default: 0,
+    },
+    emissiveIntensity: {
+      type: Number,
+      default: 1,
+    },
+    metalness: {
+      type: Number,
+      default: 0,
+    },
+    roughness: {
+      type: Number,
+      default: 1,
+    },
+  },
   created() {
-    this.material = new MeshStandardMaterial({
-      color: this.color,
-    });
+    this.material = new MeshStandardMaterial(this.propsValues());
   },
 };
