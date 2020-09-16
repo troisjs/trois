83168ee5958bf3955dd98de29753600eeec20be8
Kevin Levron <kevin.levron@gmail.com>
2020-09-16 21:11:45 +0200

    add lerpv3

--- src/tools.js
+++ src/tools.js
@@ -11,3 +11,9 @@ export function lerp(value1, value2, amount) {
   amount = amount > 1 ? 1 : amount;
   return value1 + (value2 - value1) * amount;
 };
+
+export function lerpv3(v1, v2, amount) {
+  v1.x = lerp(v1.x, v2.x, amount);
+  v1.y = lerp(v1.y, v2.y, amount);
+  v1.z = lerp(v1.z, v2.z, amount);
+};
