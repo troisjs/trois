5320cbe7dc9c2a664b4d7891ad396274d4eb67f2
Kevin Levron <kevin.levron@gmail.com>
2020-10-04 00:07:43 +0200

    add map and envmap

--- src/materials/index.js
+++ src/materials/index.js
@@ -5,3 +5,6 @@ export { default as PhysicalMaterial } from './PhysicalMaterial.js';
 export { default as StandardMaterial } from './StandardMaterial.js';
 export { default as SubSurfaceMaterial } from './SubSurfaceMaterial.js';
 export { default as ToonMaterial } from './ToonMaterial.js';
+
+export { default as Map } from './Map.js';
+export { default as EnvMap } from './EnvMap.js';
