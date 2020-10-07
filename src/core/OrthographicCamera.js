50af37552bbc3d45d11bd714ef5b298a88a0cae8
Kevin Levron <kevin.levron@gmail.com>
2020-10-07 23:38:39 +0200

    update camera

--- src/core/OrthographicCamera.js
+++ src/core/OrthographicCamera.js
@@ -1,21 +1,24 @@
-import { PerspectiveCamera, Vector3 } from 'three';
+import { OrthographicCamera, Vector3 } from 'three';
 import { watch } from 'vue';
 import useBindProp from '../use/useBindProp.js';
 
 export default {
   inject: ['three'],
   props: {
-    aspect: { type: Number, default: 1 },
-    far: { type: Number, default: 2000 },
-    fov: { type: Number, default: 50 },
+    left: { type: Number, default: -1 },
+    right: { type: Number, default: 1 },
+    top: { type: Number, default: 1 },
+    bottom: { type: Number, default: -1 },
     near: { type: Number, default: 0.1 },
+    far: { type: Number, default: 2000 },
+    zoom: { type: Number, default: 1 },
     position: { type: [Object, Vector3], default: { x: 0, y: 0, z: 0 } },
   },
   created() {
-    this.camera = new PerspectiveCamera(this.fov, this.aspect, this.near, this.far);
+    this.camera = new OrthographicCamera(this.left, this.right, this.top, this.bottom, this.near, this.far);
     useBindProp(this, 'position', this.camera.position);
 
-    ['aspect', 'far', 'fov', 'near'].forEach(p => {
+    ['left', 'right', 'top', 'bottom', 'near', 'far', 'zoom'].forEach(p => {
       watch(() => this[p], () => {
         this.camera[p] = this[p];
         this.camera.updateProjectionMatrix();
@@ -27,5 +30,5 @@ export default {
   render() {
     return [];
   },
-  __hmrId: 'PerspectiveCamera',
+  __hmrId: 'OrthographicCamera',
 };
