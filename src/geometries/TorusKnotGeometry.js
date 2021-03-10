2fd469bd20c7e5c570123f87265f71ea37e41992
Kevin Levron <kevin.levron@gmail.com>
2021-03-10 22:05:01 +0100

    fix TorusKnotGeometry

--- src/geometries/TorusKnotGeometry.js
+++ src/geometries/TorusKnotGeometry.js
@@ -6,14 +6,14 @@ export default {
   props: {
     radius: { type: Number, default: 1 },
     tube: { type: Number, default: 0.4 },
-    radialSegments: { type: Number, default: 64 },
-    tubularSegments: { type: Number, default: 8 },
+    tubularSegments: { type: Number, default: 64 },
+    radialSegments: { type: Number, default: 8 },
     p: { type: Number, default: 2 },
     q: { type: Number, default: 3 },
   },
   methods: {
     createGeometry() {
-      this.geometry = new TorusKnotGeometry(this.radius, this.tube, this.radialSegments, this.tubularSegments, this.p, this.q);
+      this.geometry = new TorusKnotGeometry(this.radius, this.tube, this.tubularSegments, this.radialSegments, this.p, this.q);
     },
   },
 };
