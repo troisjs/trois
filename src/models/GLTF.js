d8cc705b99e303c5c16277e7ec774b8fa3e23d95
Kevin Levron <kevin.levron@gmail.com>
2021-03-13 16:59:53 +0100

    improve gltf model

--- src/models/GLTF.js
+++ src/models/GLTF.js
@@ -3,13 +3,14 @@ import Object3D from '../core/Object3D.js';
 
 export default {
   extends: Object3D,
+  emits: ['loaded'],
   props: {
     src: String,
-    cameraPosition: Object,
   },
   created() {
     const loader = new GLTFLoader();
     loader.load(this.src, (gltf) => {
+      this.$emit('loaded', gltf);
       this.initObject3D(gltf.scene);
     });
   },
