27969ea3bc45f30e489b98fa38588234a56e4401
Kevin Levron <kevin.levron@gmail.com>
2021-03-16 21:54:48 +0100

    export Object3D

--- src/core/index.js
+++ src/core/index.js
@@ -4,3 +4,4 @@ export { default as PerspectiveCamera } from './PerspectiveCamera.js';
 export { default as Camera } from './PerspectiveCamera.js';
 export { default as Group } from './Group.js';
 export { default as Scene } from './Scene.js';
+export { default as Object3D } from './Object3D.js';
