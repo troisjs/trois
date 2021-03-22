68d7bd19ea9819dbb2f5d37710c98922cb0b1fef
Sander Moolin <sandermoolin@gmail.com>
2021-03-22 14:56:24 -0400

    wip on raycaster

--- src/core/Raycaster.js
+++ src/core/Raycaster.js
@@ -1,18 +1,127 @@
 import { watch } from 'vue';
 import { bindProp } from '../tools.js';
+import { Raycaster, Vector2 } from 'three'
 
 export default {
   name: 'Raycaster',
   inject: ['three'],
-  emits: ['created', 'ready'],
+  // emits: ['created', 'ready'],
   props: {
+    onBeforeRender: {
+      type: Function,
+      default: null
+    },
+    onPointerEnter: {
+      type: Function,
+      default: null
+    },
+    onPointerLeave: {
+      type: Function,
+      default: null
+    },
+    onPointerOver: {
+      type: Function,
+      default: null
+    },
+    scene: {
+      type: Object,
+      default: null
+    },
+    // user-provided camera. defaults to first parent camera
+    camera: {
+      type: Object,
+      default: null
+    },
+    intersects: {
+      type: Array,
+      default: null
+    }
   },
-  // can't use setup because it will not be used in sub components
-  // setup() {},
+  setup() {
+    const raycaster = new Raycaster();
+    const mouse = new Vector2();
+
+    return { mouse, raycaster }
+  },
+  data() {
+    return {
+      raycasterCamera: null
+    }
+  },
   mounted() {
-    console.log('TODO: raycaster')
+    // prep non-reactive list of intersections
+    this._intersects = []
+
+    // save camera if we don't already have one
+    if (!this.camera) {
+      let parent = this.$parent
+      while (parent && !this.raycasterCamera) {
+        this.raycasterCamera = parent.camera
+        parent = parent.$parent
+      }
+    }
+
+    // add update method
+    this.three.onBeforeRender(this.update)
   },
   methods: {
+    update() {
+      // custom callback
+      if (this.onBeforeRender) {
+        this.onBeforeRender(this.raycaster);
+        return;
+      }
+
+      // save scene
+      const scene = this.scene || this.three.scene;
+      if (!scene) {
+        console.log('No scene detected');
+        return;
+      }
+
+      // run standard camera-based raycaster...
+      this.raycaster.setFromCamera(this.mouse, this.raycasterCamera);
+      const intersects = this.raycaster.intersectObjects(
+        // ...against either our predefined objects or all objects in scene
+        this.intersects || scene.children
+      );
+
+      // capture new intersects
+      if (this.onPointerEnter) {
+        const old = this._intersects.map(intersect => {
+          return {
+            object: intersect.object,
+            instanceId: intersect.instanceId
+          }
+        });
+        const newIntersects = intersects.filter(intersect => !old.find(val => val.object === intersect.object && val.instanceId === intersect.instanceId));
+        if (newIntersects.length) {
+          this.onPointerEnter(newIntersects)
+        }
+      }
+
+      // capture current intersects
+      if (this.onPointerOver) {
+        this.onPointerOver(intersects)
+      }
+
+      // capture expired intersects
+      if (this.onPointerLeave) {
+        const newObjects = intersects.map(intersect => {
+          return {
+            object: intersect.object,
+            instanceId: intersect.instanceId
+          }
+        });
+        const expiredIntersects = this._intersects.filter(intersect => !newObjects.find(val => val.object === intersect.object && val.instanceId === intersect.instanceId));
+        if (expiredIntersects.length) {
+          this.onPointerLeave(expiredIntersects)
+        }
+      }
+
+      // save internal intersect list
+      this._intersects = intersects;
+    }
   },
   render() {
     return this.$slots.default ? this.$slots.default() : [];
