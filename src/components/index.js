e5fa85515fac8e355d9e32a93ba5a21ce257aed3
Kevin Levron <kevin.levron@gmail.com>
2021-04-22 01:00:06 +0200

    cleanup

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as NoisyImage } from './noisy/NoisyImage.js';
-export { default as NoisyPlane } from './noisy/NoisyPlane.js';
-export { default as NoisySphere } from './noisy/NoisySphere.js';
-export { default as NoisyText } from './noisy/NoisyText.js';
-
-export { default as Slider1 } from './sliders/Slider1.vue';
-export { default as Slider2 } from './sliders/Slider2.vue';
-
-export { default as GLTFViewer } from './viewers/GLTFViewer.vue';
