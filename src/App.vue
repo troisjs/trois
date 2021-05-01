1c4acdc1e380c465926a21f1c047342580b49a68
Kevin Levron <kevin.levron@gmail.com>
2021-05-01 12:27:09 +0200

    update app example

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Box from './meshes/Box'
-import Camera from './core/PerspectiveCamera'
-import LambertMaterial from './materials/LambertMaterial'
-import { MeshInterface } from './meshes/Mesh'
-import PointLight from './lights/PointLight'
-import Renderer, { RendererInterface } from './core/Renderer'
-import Scene from './core/Scene'
+import { Box, Camera, LambertMaterial, MeshInterface, PointLight, Renderer, RendererInterface, Scene } from './export'
 
 export default defineComponent({
   components: { Box, Camera, LambertMaterial, PointLight, Renderer, Scene },
