36f9db4f62672e94eec9f68f56a4c8b842a63f83
YangLei <xcchcaptain@126.com>
2021-11-04 15:32:23 +0800

    fix Gltf lost animation data

--- src/models/GLTF.ts
+++ src/models/GLTF.ts
@@ -1,13 +1,19 @@
-import { defineComponent } from 'vue'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import Model from './Model'
+import { defineComponent } from "vue";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import Model from "./Model";
 
 export default defineComponent({
   extends: Model,
   created() {
-    const loader = new GLTFLoader()
-    loader.load(this.src, (gltf) => {
-      this.onLoad(gltf.scene)
-    }, this.onProgress, this.onError)
+    const loader = new GLTFLoader();
+    loader.load(
+      this.src,
+      (gltf) => {
+        gltf.scene.animations = gltf.animations;
+        this.onLoad(gltf.scene);
+      },
+      this.onProgress,
+      this.onError
+    );
   },
-})
+});
