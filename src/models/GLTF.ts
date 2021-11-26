07064c32a34dcc18425482058be693d78480cc27
Georg Nagel <g.schlmm@gmail.com>
2021-11-26 11:20:29 +0100

    Models: add before-load event

--- src/models/GLTF.ts
+++ src/models/GLTF.ts
@@ -6,6 +6,7 @@ export default defineComponent({
   extends: Model,
   created() {
     const loader = new GLTFLoader()
+    this.$emit('before-load', loader)
     loader.load(this.src, (gltf) => {
       this.onLoad(gltf.scene)
     }, this.onProgress, this.onError)
