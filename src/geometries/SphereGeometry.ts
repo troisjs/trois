fee0fcf9b65101d96941623e01b99884179a6327
Ken V <kenverhaegen@gmail.com>
2022-01-22 04:18:40 +0100

    Add missing SphereGeometry parameters

--- src/geometries/SphereGeometry.ts
+++ src/geometries/SphereGeometry.ts
@@ -5,6 +5,10 @@ export const props = {
   radius: { type: Number, default: 1 },
   widthSegments: { type: Number, default: 12 },
   heightSegments: { type: Number, default: 12 },
+  phiStart: { type: Number, default: 0 },
+  phiLength: { type: Number, default: Math.PI * 2 },
+  thetaStart: { type: Number, default: 0 },
+  thetaLength: { type: Number, default: Math.PI },
 } as const
 
 export function createGeometry(comp: any): SphereGeometry {
