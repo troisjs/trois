02e4219660f6410059685cd43be1579f07cea7f3
Alvaro <alvaro.saburido@gmail.com>
2022-01-24 17:30:34 +0100

    feat: added alias and factory to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import * as path from 'path'
 
-export default {
-  plugins: [vue()]
-};
+export default defineConfig({
+  resolve: {
+    alias: {
+      '/@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [vue()],
+})
