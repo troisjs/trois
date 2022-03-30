bdcd9b7c49a8a5495897fad3695a9b9c105b7748
Yang <weiyang@studiomultiply.com>
2022-03-30 17:28:03 +0800

    gltf import test

--- src/components/archviz/YangArchvizComponent.ts
+++ src/components/archviz/YangArchvizComponent.ts
@@ -1,40 +1,33 @@
 import { defineComponent, h } from "vue";
 import { PerspectiveCamera, Renderer, Scene } from "../../core";
-import { PointLight } from "../../lights";
-import { LambertMaterial } from "../../materials";
-import { Box } from "../../meshes";
+import { AmbientLight } from "../../lights";
+import { GltfModel } from "../../models";
 
 export default defineComponent({
     name: "YangArchvizComponent",
-    components: {
-        PerspectiveCamera, 
-        Renderer, 
-        Scene,
-        PointLight,
-        LambertMaterial,
-        Box,
+    props: {
+        gltfUrl : String
     },
-    setup(props, { slots }){
+    components: { },
+    setup(props, { slots, expose }){
+        const url : String = props.gltfUrl ?? "";
+
+        const gltfProps = {
+            src : url
+        }
+
+        //@ts-ignore
+        const gltfVNode = h( GltfModel, gltfProps );
 
         return () =>h( Renderer, {  
                         resize : "window" ,  
+                        alpha : true
                     }, () => 
-                    [
-                        h( PerspectiveCamera, {
-                            position: { z: 10 }
-                        }),
+                    [ 
                         h( Scene , {}, () =>
-                        [
-                            h( PointLight, {
-                                position: { y: 50, z: 50 }
-                            }),
-                            h( Box, {
-                                size : 1,
-                                rotation : { y: Math.PI / 4, z: Math.PI / 4 },
-                            }, () =>
-                            [
-                                h( LambertMaterial )
-                            ])
+                        [ 
+                            h( AmbientLight ),
+                            gltfVNode
                         ])
                     ]);
     }, 
